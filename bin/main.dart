c0db4f39138f1fe450ad2588fc3cf652fc3d099e
Jake Mathews <mathewsj2@wit.edu>
2018-05-01 14:43:48 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+main(final List<String> arguments) {
+  if (arguments.length != 1) {
+    print("Incorrect number of arguments. Please supply path to probe results");
+    return;
+  }
+
+  print(arguments);
+}
---
+++ lib/probe_parser.dart
@@ -0,0 +1,7 @@
+import 'package:probe_report/probe_report.dart';
+
+class ProbeParser {
+  ProbeReport parse(final String probeResults) {
+    return null;
+  }
+}
