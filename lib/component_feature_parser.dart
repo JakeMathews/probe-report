76524075ea396d6121935701a6f6385916910f51
Jake Mathews <mathewsj2@wit.edu>
2018-05-14 11:31:43 -0400

    Added section sepration to ProbeReportParser

--- lib/component_feature_parser.dart
+++ lib/component_feature_parser.dart
@@ -1,1 +1,7 @@
-class ComponentFeatureParser {}
+import 'package:probe_report/model/component.dart';
+
+class ComponentFeatureParser {
+  Component parse(final String componentText) {
+    return null;
+  }
+}
--- lib/probe_report_parser.dart
+++ lib/probe_report_parser.dart
@@ -1,4 +1,5 @@
 import 'package:probe_report/component_feature_parser.dart';
+import 'package:probe_report/model/component.dart';
 import 'package:probe_report/model/probe_report.dart';
 
 class ProbeReportParser {
@@ -9,7 +10,27 @@ class ProbeReportParser {
   ProbeReport parse(final String probeResults) {
     final ProbeReport probeReport = new ProbeReport();
 
-    for (final String line in probeResults.split('\n')) {}
+    var lineBreakCount = 0;
+    var stringBuffer = new StringBuffer();
+    for (final String line in probeResults.split('\n')) {
+      var isLineBreak = line.contains('-' * 67);
+      if (isLineBreak) {
+        lineBreakCount++;
+      }
+
+      if (lineBreakCount <= 0) {
+        continue;
+      } else if (lineBreakCount >= 3) {
+        final Component component = componentFeatureParser.parse(stringBuffer.toString());
+        if (probeReport.components.containsKey(component.componentNumber)) {
+          probeReport.components[component.componentNumber].features.addAll(component.features);
+        }
+        stringBuffer.clear();
+        lineBreakCount = 1;
+      }
+
+      stringBuffer.writeln(line);
+    }
 
     return probeReport;
   }
